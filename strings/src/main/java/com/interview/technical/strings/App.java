fdb52e92c1f8621af035971ed22690921ecad35d
Sathish Ganesan <satganes@gmail.com>
2018-03-04 22:59:13 +0530

    Initial commit to kickstart my files

---
+++ strings/src/main/java/com/interview/technical/strings/App.java
@@ -0,0 +1,13 @@
+package com.interview.technical.strings;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

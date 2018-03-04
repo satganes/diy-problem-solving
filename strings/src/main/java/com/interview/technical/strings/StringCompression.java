3dd13cdbcb3ea01f051435afce82e11a20f129b8
Sathish Ganesan <satganes@gmail.com>
2018-03-04 23:36:37 +0530

    Template Checkin

---
+++ strings/src/main/java/com/interview/technical/strings/StringCompression.java
@@ -0,0 +1,16 @@
+package com.interview.technical.strings;
+
+/**
+ * Implement a method to perform basic string compression using the counts of
+ * repeated characters. For example, the string aabcccccaaa would become
+ * a2blc5a3. If the "compressed" string would not become smaller than the
+ * original string, your method should return the original string. You can
+ * assume the string has only uppercase and lowercase letters (a - z).
+ * 
+ * @author Sathish Ganesan
+ *
+ */
+public class StringCompression
+{
+
+}
---
+++ strings/src/main/java/com/interview/technical/strings/ZeroMatrix.java
@@ -0,0 +1,13 @@
+package com.interview.technical.strings;
+
+/**
+ * Write an algorithm such that if an element in an MxN matrix is 0, its entire
+ * row and column are set to 0.
+ * 
+ * @author Sathish Ganesan
+ *
+ */
+public class ZeroMatrix
+{
+
+}

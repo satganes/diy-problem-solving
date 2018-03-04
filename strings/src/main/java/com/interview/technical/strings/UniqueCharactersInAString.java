4a360916e0b4fd9c072e88b26a3ed005b010edf0
Sathish Ganesan <satganes@gmail.com>
2018-03-04 23:12:04 +0530

    UniqueCharacters In A String

---
+++ strings/src/main/java/com/interview/technical/strings/UniqueCharactersInAString.java
@@ -0,0 +1,14 @@
+package com.interview.technical.strings;
+
+/**
+ * Implement an algorithm to determine if a string has all unique characters.
+ * What if you cannot use additional data structures?
+ *
+ */
+public class UniqueCharactersInAString
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}

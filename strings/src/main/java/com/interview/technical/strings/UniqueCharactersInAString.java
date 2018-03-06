ef0018f659f10d4cb9b05da5af4d516688e30f82
Sathish Ganesan <satganes@gmail.com>
2018-03-06 15:16:41 +0530

    Solutions for 2 more

--- strings/src/main/java/com/interview/technical/strings/UniqueCharactersInAString.java
+++ strings/src/main/java/com/interview/technical/strings/UniqueCharactersInAString.java
@@ -1,14 +1,41 @@
 package com.interview.technical.strings;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Implement an algorithm to determine if a string has all unique characters.
- * What if you cannot use additional data structures?
+ * Implement an algorithm to determine if a string has all unique characters. What if you cannot use
+ * additional data structures?
  *
  */
 public class UniqueCharactersInAString
 {
+    public static void bruteForce(String input)
+    {
+        char[] sequence = input.toCharArray();
+        List<Character> duplicates = new ArrayList<>();
+        for (int i = 0; i < sequence.length; i++)
+        {
+            for (int j = i + 1; j < sequence.length; j++)
+            {
+                if (sequence[i] == sequence[j])
+                {
+                    duplicates.add(sequence[i]);
+                }
+            }
+        }
+        if (!duplicates.isEmpty())
+        {
+            System.out.println("Found duplicates: " + duplicates.toString());
+        }
+        else
+        {
+            System.out.println("Unique");
+        }
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        UniqueCharactersInAString.bruteForce("abcd");
     }
 }

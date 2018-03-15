8b7a739ec96d12307d158341194ca3733b331bd8
Sathish Ganesan <satganes@gmail.com>
2018-03-15 23:21:06 +0530

    INternal package for linked lists

--- strings/src/main/java/com/interview/technical/crackingthecodinginterview/linkedlists/LoopDetection.java
+++ strings/src/main/java/com/interview/technical/crackingthecodinginterview/linkedlists/LoopDetection.java
@@ -6,12 +6,14 @@ package com.interview.technical.crackingthecodinginterview.linkedlists;
  * returns the node at the beginning of the loop.
  * 
  * <pre>
+ * 
  * DEFINITION Circular linked
  * list: A (corrupt) linked list in which a node's next pointer points to an
  * earlier node, so as to make a loop in the linked list. 
  * EXAMPLE 
  * Input: A -> B -> C - > D -> E -> C [the same C as earlier] 
  * Output: C
+ * 
  * </pre>
  * 
  * @author Sathish Ganesan

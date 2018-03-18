8d97fc86d831f7ced86b9295a1bd73e45bdf3a71
Sathish Ganesan <satganes@gmail.com>
2018-03-19 01:46:35 +0530

    Remove middle element in linkedlist

--- crackingthecodinginterview/src/main/java/com/interview/technical/crackingthecodinginterview/linkedlists/RemoveMiddleElement.java
+++ crackingthecodinginterview/src/main/java/com/interview/technical/crackingthecodinginterview/linkedlists/RemoveMiddleElement.java
@@ -1,5 +1,8 @@
 package com.interview.technical.crackingthecodinginterview.linkedlists;
 
+import com.interview.technical.crackingthecodinginterview.linkedlists.internal.MyLinkedList;
+import com.interview.technical.crackingthecodinginterview.linkedlists.internal.Node;
+
 /**
  * 
  * Implement an algorithm to delete a node in the middle (i.e., any node but the
@@ -20,5 +23,34 @@ package com.interview.technical.crackingthecodinginterview.linkedlists;
  */
 public class RemoveMiddleElement
 {
+    public static void delete(MyLinkedList input)
+    {
+        Node slow = input.head;
+        Node fast = input.head;
+        Node slowMinusOne = null;
+        while (fast != null && fast.next != null)
+        {
+            slowMinusOne = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slowMinusOne.next = slow.next;
+    }
+
+    public static void main(String[] args)
+    {
+        MyLinkedList input = new MyLinkedList();
+        input.push(1);
+        input.push(2);
+        input.push(3);
+        input.push(4);
+        input.push(5);
+        input.push(6);
+        input.push(7);
+        input.push(8);
+        System.out.println("Before : " + input.print());
+        RemoveMiddleElement.delete(input);
+        System.out.println("After : " + input.print());
+    }
 
 }
